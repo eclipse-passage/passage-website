18aa0b6d514803f295132236d46e6812eac53611
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2019-04-05 11:21:10 +0300

    [Passage] move wiki link
    Change-Id: Idc8012e1f98d3d78c2480e03c108b9bfc80fdc6c
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

--- index.php
+++ index.php
@@ -16,63 +16,70 @@
 	$html = <<<EOHTML
 	
 <div id="midcolumn">
-	<h3>Eclipse Passage (License Management Solution)</h3>
+	<a href="https://wiki.eclipse.org/Passage"><h3>Eclipse Passage (License Management Solution)</h3></a>
 	<div id="introText">
-
-	<p>
-	The Eclipse Passage project aims to provide rich and easily adaptable capabilities to define and control licensing constraints.
-	</p>
+		<p>
+		The Eclipse Passage project aims to provide rich and easily adaptable capabilities to define and control licensing constraints.
+		</p>
+		
+		<div id="community">
+		<p/>
+		We will be happy to help with integration, you are welcome to:
+		<p/>
+		<li><a href="https://www.eclipse.org/forums/index.php/f/449/">initiate discussion</a></li>
+		<li><a href="https://dev.eclipse.org/mhonarc/lists/passage-dev">contact developers</a></li>
+		<li><a href="https://bugs.eclipse.org/bugs/enter_bug.cgi?product=passage&component=General">or request a change</a></li>
+		</p>
+		</p>
+		</div>
 	
-	<p/>
-	We will be happy to help with integration, you are welcome to:
-	<p/>
-	<li><a href="https://www.eclipse.org/forums/index.php/f/449/">initiate discussion</a></li>
-	<li><a href="https://dev.eclipse.org/mhonarc/lists/passage-dev">contact developers</a></li>
-	<li><a href="https://bugs.eclipse.org/bugs/enter_bug.cgi?product=passage&component=General">or request a change</a></li>
-	</p>
-	</p>
-
-	<p/>
-	The Eclipse Passage represents several sets of components for license management:
-	<p/>
-	<li>Licensing Runtime Interfaces</li>
-	<li>Licensing Operator Client</li>
-	<li>Licensing Back-end Server</li>
-	</p>
-	</p>
-
-	Eclipse Passage is primary focused to support OSGi-based products:
-	<li>Capability-based licensing constraints definitions</li>
-	<li>Component-based licensing constraints definitions</li>
-	<li>More to come</li>
-
-	Easy to use, simple to integrate, cheap to maintain.
-
-	</p>
-<img
-	class="displayed"
-	src="/passage/images/snapshot.png"
-	height="550"
-	alt="Eclipse Passage Snapshots"
-	border="0"/>
-</div>
+		<div id="components">
+		<p/>
+		The Eclipse Passage represents several sets of components for license management:
+		<p/>
+		<li>Licensing Runtime Interfaces</li>
+		<li>Licensing Operator Client</li>
+		<li>Licensing Back-end Server</li>
+		</p>
+		</p>
+		</div>
+	
+		<div id="functionality">
+		<p/>
+		Eclipse Passage is primary focused to support OSGi-based products:
+		<p/>
+		<li>Capability-based licensing constraints definitions</li>
+		<li>Component-based licensing constraints definitions</li>
+		<li>More to come</li>
+		</p>
+		</p>
+		</div>
+	
+		Easy to use, simple to integrate, cheap to maintain.
+	
+		</p>
+		<img
+			class="displayed"
+			src="/passage/images/snapshot.png"
+			height="550"
+			alt="Eclipse Passage Snapshots"
+			border="0"/>
+	</div>
 </div>
 
 <div id="rightcolumn">
 	<div id="headlines">
-		<a href="https://wiki.eclipse.org/Passage">
-			<h3>Eclipse Passage Wiki</h3>
-		</a>
 		<p>
 			<a href="https://www.eclipse.org/projects/what-is-incubation.php">
 				<img class="project-image center-block margin-bottom-20 img-responsive" typeof="foaf:Image" src="https://projects.eclipse.org/sites/all/modules/custom/pmi/project_state/images/incubating.png" alt="Incubating - Eclipse Passage">
 			</a>
 			<p/>
 			<a href="https://projects.eclipse.org/projects/technology.passage/releases/0.4.0">
-				<i>The Eclipse Passage 0.4.0 (available)</i>
+				<i>Eclipse Passage 0.4.0 (available)</i>
 			</a>
+			<p/>
 			<a href="https://projects.eclipse.org/projects/technology.passage/releases/0.5.0">
-				<i>The Eclipse Passage 0.5.0 (planned)</i>
+				<i>Eclipse Passage 0.5.0 (planned)</i>
 			</a>
 		</p>
 	</div>
