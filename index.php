bcf5a4dc5fc1054093ba008d9f4c4503ea62890a
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2019-11-02 15:22:28 +0300

    Add information regarding 0.7.0
    Change-Id: Ie7eafad6b38f1fd1a9087c005e37f2554ffb6284
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

--- index.php
+++ index.php
@@ -71,6 +71,9 @@
 <div id="rightcolumn">
 	<div id="headlines">
 		<p>
+			<a href="https://projects.eclipse.org/projects/technology.passage/releases/0.7.0">
+				<i>Eclipse Passage 0.7.0 (in progress)</i>
+			</a>
 			<a href="https://projects.eclipse.org/projects/technology.passage/releases/0.6.0">
 				<i>Eclipse Passage 0.6.0 (available)</i>
 			</a>
